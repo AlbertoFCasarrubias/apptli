0ca568226508f7d7c9766cc02dd8e7b65ceb5fb1
Alberto Fuentes <yaotzin68@gmail.com>
2020-03-23 15:41:21 -0600

    chedule

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD1RntUaS60yYJIWSVayKmOSfcROhKeqfc',
+    authDomain: 'apptli-397cb.firebaseapp.com',
+    databaseURL: 'https://apptli-397cb.firebaseio.com',
+    projectId: 'apptli-397cb',
+    storageBucket: 'apptli-397cb.appspot.com',
+    messagingSenderId: '431566778438',
+    appId: '1:431566778438:web:f42969a4257fe63b6ada47',
+    measurementId: 'G-KPJ3EN8LZT'
+  },
+  EVENT_STATUS: {
+    pay: 'pay',
+    cancelled: 'cancelled',
+    requestByDoctor: 'requestByDoctor',
+    requestByPatient: 'requestByPatient',
+    approved: 'approved',
+    unpay: 'unpay',
+  }
 };
